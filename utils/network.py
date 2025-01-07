002b98dac576a944e276781547c6aefeb43fe1e0
Karthik Desingu <karthikdesingu2000@gmail.com>
2025-01-07 02:05:35 -0500

    fix node ordering

--- utils/network.py
+++ utils/network.py
@@ -17,13 +17,12 @@ def get_graph(adjacency_matrix, hub_nodes=[], node_posns=None):
 	
 	# make AGraph; set attributes for hub nodes.
 	agr = to_agraph(gr)
-	print(agr.directed)
 	agr.graph_attr['size'] = 100
 	agr.graph_attr['ratio'] = 1
 	for i, node in enumerate(agr.iternodes()):
-		node.attr['label'] = f"C{str(node)}"
+		node.attr['label'] = f"C{i+1}"
 		node.attr['style'] = 'filled'
-		node.attr['width'], node.attr['height'] = 2, 2
+		node.attr['width'], node.attr['height'] = 1.5, 1.5
 		node.attr['fontsize'] = 80
 		if int(str(node)) in hub_nodes:
 			node.attr['color'] = 'red'
@@ -36,6 +35,18 @@ def get_graph(adjacency_matrix, hub_nodes=[], node_posns=None):
 		# set node positions if supplied.
 		if node_posns is not None:
 			node.attr['pos'] = f"{node_posns[i][0]},{node_posns[i][1]}!"
+
+	for i, edge in enumerate(agr.iteredges()):
+		source, target = edge
+		edge.attr['dir'] = "both"
+		edge.attr['arrowsize'] = 5
+		edge.attr['arrowtail'] = "tee"
+		if int(str(source)) in hub_nodes:
+			edge.attr['color'] = 'red'
+			edge.attr['penwidth'] = 2.5
+		else:
+			edge.attr['color'] = 'black'
+			edge.attr['penwidth'] = 2
 
 	_ = agr.layout(prog='sfdp') if node_posns is None else agr.layout(prog='neato')
 	return agr
