71d07b7a45dd816cc93c04b08f9834b94536d8ce
Karthik Desingu <karthikdesingu2000@gmail.com>
2025-06-26 16:47:30 -0400

    optimize global plotters

---
+++ B3_global_average.py
@@ -0,0 +1,96 @@
+import glob 
+import os
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot as plot
+
+
+levels_l = glob.glob(os.path.join(
+	"/home/kd766/quorum-sensing/outputs/subexp",
+	"*/*levels_final.csv*"
+))
+
+clouds_l = glob.glob(os.path.join(
+	"/home/kd766/quorum-sensing/outputs/subexp",
+	"*/*clouds_final.csv*"
+))
+
+
+# TODO: merge the two for-loops.
+
+
+density_d = dict()
+level_means_d = dict()
+cloud_means_d = dict()
+levels_fig = plot.figure(1, figsize=(16, 16))
+clouds_fig = plot.figure(2, figsize=(16, 16))
+for idx, (cloud_file, level_file) in enumerate(zip(clouds_l, levels_l)):
+
+	fname = os.path.basename(level_file)
+	parts = fname.split('_')
+	density = float(parts[3].split('-')[-1])
+	density_str = str(round(density, 4))
+
+	cloud = pd.read_csv(cloud_file, header=None).to_numpy()
+	levels = pd.read_csv(level_file, header=None).to_numpy()
+	n_cells = np.sum(levels!=0)
+
+	cloud_means_d[density_str] = cloud_means_d.get(density_str, []) + [np.sum(cloud)/n_cells]
+	level_means_d[density_str] = level_means_d.get(density_str, []) + [np.sum(levels)/n_cells]
+	density_d[density_str] = density
+
+	plot.figure(levels_fig)
+	ax = plot.subplot(7, 8, idx+1)
+	ax.hist(levels.flatten(), bins=8)
+	ax.set_title(f"density = {round(density, 4)}")
+	ax.set_yscale('log')
+
+	plot.figure(clouds_fig)
+	ax = plot.subplot(7, 8, idx+1)
+	ax.hist(clouds.flatten(), bins=8)
+	ax.set_title(f"density = {round(density, 4)}")
+	ax.set_yscale('log')
+
+plot.figure(levels_fig)
+plot.tight_layout()
+plot.savefig(os.path.join(
+	'analysis_outputs', f'global-mean_levels_hist.png'), dpi=100)
+
+plot.figure(clouds_fig)
+plot.tight_layout()
+plot.savefig(os.path.join(
+	'analysis_outputs', f'global-mean_clouds_hist.png'), dpi=100)
+
+
+# plot levels trend.
+plot.figure(figsize=(10, 10))
+for k in level_means_d.keys():
+	trials_mean = np.mean(level_means_d[k])
+	trials_std = np.std(level_means_d[k])
+	# plot centrality measures.
+	plot.errorbar(density_d[k], trials_mean, yerr=trials_std, ecolor='red', color='blue')
+	# plot all pts.
+	for signal_mean in level_means_d[k]:
+		plot.scatter(density_d[k], signal_mean, s=0.8, c='green', marker='x')
+
+plot.xlabel("cell seeding density")
+plot.ylabel("global mean levels")
+plot.savefig(os.path.join(
+	'analysis_outputs', f'global-density-mean_levels.png'), dpi=100)
+
+
+# plot clouds trend.
+plot.figure(figsize=(10, 10))
+for k in cloud_means_d.keys():
+	trials_mean = np.mean(cloud_means_d[k])
+	trials_std = np.std(cloud_means_d[k])
+	# plot centrality measures.
+	plot.errorbar(density_d[k], trials_mean, yerr=trials_std, ecolor='red', color='blue')
+	# plot all pts.
+	for signal_mean in cloud_means_d[k]:
+		plot.scatter(density_d[k], signal_mean, s=0.8, c='green', marker='x')
+
+plot.xlabel("cell seeding density")
+plot.ylabel("global mean cloud")
+plot.savefig(os.path.join(
+	'analysis_outputs', f'global-density-mean_clouds.png'), dpi=100)
